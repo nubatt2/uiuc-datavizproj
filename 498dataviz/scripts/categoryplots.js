55dc0f89839195fbcbfb03731bcaba55a0acc134
Naseer Ahmed <nubatt2@illinois.edu>
2018-07-28 03:16:05 -0700

    push latest changes

--- 498dataviz/scripts/categoryplots.js
+++ 498dataviz/scripts/categoryplots.js
@@ -41,9 +41,19 @@ svg.append("text")
     .style("text-anchor", "middle")
     .style("font", "11px sans-serif")
     .style("padding-top", "12px")
-    .text("Expected Gaming to be dominant comparatively.");
+    .text("Gaming is not dominant category.");
 svg.append("line").attr("x1", 800).attr("x2", 890).attr("y1", 300).attr("y2", 550).attr("stroke", "red");
 
+// annotation : text + line
+svg.append("text")
+    .attr("transform", "translate(" + 280 + " ," + 100 + ")")
+    .style("text-anchor", "middle")
+    .style("font", "11px sans-serif")
+    .style("padding-top", "12px")
+    .html("As expected, more videos under entertainment than others.");
+svg.append("line").attr("x1", 270).attr("x2", 120).attr("y1", 100).attr("y2", 170).attr("stroke", "red");
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 var svg_uk = d3.select("#uk_cat_svg");
 var g_uk = svg_uk.append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
@@ -73,30 +83,39 @@ svg_uk.append("text")
     .style("text-anchor", "middle")
     .style("font", "11px sans-serif")
     .style("padding-top", "12px")
-    .html("Gaming is dominant comparatively.");
+    .html("Gaming is dominant compared to US.");
 svg_uk.append("line").attr("x1", 501).attr("x2", 501).attr("y1", 300).attr("y2", 510).attr("stroke", "red");
 
 
-// annotation. text + line
+// annotation. text + 2 lines
 svg_uk.append("text")
-    .attr("transform", "translate(" + 270 + " ," + 100 + ")")
+    .attr("transform", "translate(" + 280 + " ," + 100 + ")")
     .style("text-anchor", "middle")
     .style("font", "11px sans-serif")
     .style("padding-top", "12px")
-    .html("Compared to US, Music beats Entertainment in videos.");
+    .html("Compared to US, Music is more prominant than Entertainment.");
 svg_uk.append("line").attr("x1", 270).attr("x2", 120).attr("y1", 100).attr("y2", 190).attr("stroke", "red");
-svg_uk.append("line").attr("x1", 270).attr("x2", 190).attr("y1", 100).attr("y2", 240).attr("stroke", "blue");
+svg_uk.append("line").attr("x1", 270).attr("x2", 185).attr("y1", 100).attr("y2", 235).attr("stroke", "blue");
+
+svg_uk.append("text")
+    .attr("transform", "translate(" + 700 + " ," + 250 + ")")
+    .style("text-anchor", "middle")
+    .style("font", "11px sans-serif")
+    .style("padding-top", "12px")
+    .html("Pet's & animals is unique, this one not as famous in USA/CA.");
+svg_uk.append("line").attr("x1", 701).attr("x2", 820).attr("y1", 250).attr("y2", 545).attr("stroke", "red");
 
 
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Canada plots.
 var svg_ca = d3.select("#ca_cat_svg");
-// var margin = { top: 20, right: 20, bottom: 40, left: 100 };
-// var width = 960 - margin.left - margin.right;
-// var height = 600 - margin.top - margin.bottom;
-
 var g_ca = svg_ca.append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+let width_ca = svg.attr("width") - margin.left - margin.right;
+let height_ca = svg.attr("height") - margin.top - margin.bottom;
 
-plotCatData("./data/yt_ca_popcategories.csv", svg_ca, height, width);
+plotCatData("./data/yt_ca_popcategories.csv", svg_ca, height_ca, width_ca);
+
 // text label for the x axis
 svg_ca.append("text")
     .attr("transform", "translate(" + (width / 2) + " ," + (height + 55) + ")")
@@ -108,12 +127,33 @@ svg_ca.append("text")
 // text label for the y axis
 svg_ca.append("text")
     .attr("transform", "rotate(-90)")
-    .attr("y", 0 - margin.left)
+    .attr("y", 0 - margin.left - 30)
     .attr("x", 0 - (height / 2))
     .attr("dy", "1em")
     .style("text-anchor", "middle")
     .style("font", "18px sans-serif")
     .text(ylabel);
+
+
+// annotation. text + line
+// svg_ca.append("text")
+//     .attr("transform", "translate(" + 500 + " ," + 300 + ")")
+//     .style("text-anchor", "middle")
+//     .style("font", "11px sans-serif")
+//     .style("padding-top", "12px")
+//     .html("Gaming is dominant comparatively.");
+// svg_ca.append("line").attr("x1", 501).attr("x2", 501).attr("y1", 300).attr("y2", 510).attr("stroke", "red");
+
+// annotation. text + line
+svg_ca.append("text")
+    .attr("transform", "translate(" + 300 + " ," + 100 + ")")
+    .style("text-anchor", "middle")
+    .style("font", "11px sans-serif")
+    .style("padding-top", "12px")
+    .html("Like US, entertainment dominates,but CA stands out with News & Politics at 2nd position.");
+svg_ca.append("line").attr("x1", 270).attr("x2", 80).attr("y1", 100).attr("y2", 190).attr("stroke", "red");
+svg_ca.append("line").attr("x1", 270).attr("x2", 135).attr("y1", 100).attr("y2", 240).attr("stroke", "blue");
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 function plotCatData(file, g, height, width) {
     d3.csv(file, function (error, data) {
