ef1d4fe97794dd18e0117eb52234c538b5d85df6
mjay17 <mritunjay17@gmail.com>
2018-12-11 18:48:39 -0800

    Updating webpages and script-1

--- CS410/AmazonReviewsIntelligence/webpages/script.js
+++ CS410/AmazonReviewsIntelligence/webpages/script.js
@@ -2,6 +2,7 @@ function loadProducts() {
     loadJSON(function (response) {
         // Parsing JSON string into object
         var actual_JSON = JSON.parse(response);
+        console.log(actual_JSON);
     });
 }
 
@@ -9,7 +10,7 @@ function loadJSON(callback) {
 
     var xobj = new XMLHttpRequest();
     xobj.overrideMimeType("application/json");
-    xobj.open('GET', '../data/product_sentiments_aggregated_overall.json', true); // Replace 'appDataServices' with the path to your file
+    xobj.open('GET', '../data/product_sentiments_aggregated_overall.json', true);
     xobj.onreadystatechange = function () {
         if (xobj.readyState == 4 && xobj.status == "200") {
             // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
