c8af7d73ac1d72a3d6e2499c332bbcc35b63fda0
mjay17 <mritunjay17@gmail.com>
2018-12-12 17:27:55 -0800

    Final changes - 1

--- CS410/AmazonReviewsIntelligence/webpages/script.js
+++ CS410/AmazonReviewsIntelligence/webpages/script.js
@@ -1,16 +1,17 @@
 var product_sentiments_aggregated_overall, product_sentiments_aggregated_dates;
+var last_product_title;
 
 function loadProductsAndSentiments() {
     loadJSON(function (response) {
         // Parsing JSON string into object
         product_sentiments_aggregated_overall = JSON.parse(response);
-        var products=[];
-        for (var i = 0; i < product_sentiments_aggregated_overall.length; i++) { 
+        var products = [];
+        for (var i = 0; i < product_sentiments_aggregated_overall.length; i++) {
             products.push(product_sentiments_aggregated_overall[i].product_title);
         }
         autocomplete(document.getElementById("product_name"), products);
     }, '../data/product_sentiments_aggregated_overall.json');
-    
+
     loadJSON(function (response) {
         // Parsing JSON string into object
         product_sentiments_aggregated_dates = JSON.parse(response);
@@ -28,6 +29,57 @@ function loadJSON(callback, filePath) {
         }
     };
     xobj.send(null);
+}
+
+function showAnalytics(product_title) {
+    if (product_title && last_product_title != product_title) {
+        var product_id;
+        var product_overall_details = getProductOverallSentimentDetails(product_title);
+        if (product_overall_details.length == 1) {
+            product_id = product_overall_details[0]["product_id"];
+            document.getElementById('current_product').innerHTML = product_overall_details[0]["product_title"];
+            var sentiment_score = convertToFixedDecimalPlaces(product_overall_details[0]["overall_weighted_sentiment_score"]);
+            document.getElementById('overall_sentiment_score').innerHTML = sentiment_score + " (" + getSentimentCategory(sentiment_score) + ")";
+        }
+        var product_dates_details = getProductSentimentDetailsByDate(product_id); 
+        plotBarChart(product_dates_details);
+        last_product_title = product_title;
+    }
+}
+
+function getProductOverallSentimentDetails(product_title) {
+    return product_sentiments_aggregated_overall.filter(
+        function (data) { return data.product_title == product_title }
+    );
+}
+
+function getProductSentimentDetailsByDate(product_id) {
+    return product_sentiments_aggregated_dates.filter(
+        function (data) { return data.product_id == product_id }
+    );
+    
+}
+
+function getSentimentCategory(sentiment_score) {
+    if (sentiment_score >= -3 && sentiment_score < -2) {
+        return "Very Negative";
+    }
+    else if (sentiment_score >= -2 && sentiment_score < -1) {
+        return "Negative";
+    }
+    else if (sentiment_score >= -1 && sentiment_score <= 1) {
+        return "Neutral";
+    }
+    else if (sentiment_score > 1 && sentiment_score <= 2) {
+        return "Very Positive";
+    }
+    else if (sentiment_score > 2 && sentiment_score <= 3) {
+        return "Positive";
+    }
+}
+
+function convertToFixedDecimalPlaces(num) {
+    return num.toFixed(2);
 }
 
 function autocomplete(inp, arr) {
@@ -125,4 +177,171 @@ function autocomplete(inp, arr) {
     document.addEventListener("click", function (e) {
         closeAllLists(e.target);
     });
+}
+
+function plotBarChart(product_data) {
+    const svg = d3.select('svg');
+    svg.selectAll("*").remove();
+    const svgContainer = d3.select('#container');
+
+    const margin = 100;
+    const width = 900 - 2 * margin;
+    const height = 400 - 2 * margin;
+
+    const chart = svg.append('g')
+        .attr('transform', `translate(${margin}, ${margin})`);
+
+    const xScale = d3.scaleBand()
+        .range([0, width])
+        .domain(product_data.map((s) => s.review_date))
+        .padding(0.4)
+
+    const yScale = d3.scaleLinear()
+        .range([height, 0])
+        .domain([-3, 3]);
+
+    // vertical grid lines
+    // const makeXLines = () => d3.axisBottom()
+    //   .scale(xScale)
+
+    const makeYLines = () => d3.axisLeft()
+        .scale(yScale)
+
+    chart.append('g')
+        .attr('transform', `translate(0, ${height})`)
+        .call(d3.axisBottom(xScale))
+        .selectAll("text")	
+            .style("text-anchor", "end")
+            .attr("dx", "-.8em")
+            .attr("dy", ".15em")
+            .attr("transform", function(d) {
+                return "rotate(-90)" 
+                });
+
+
+    chart.append('g')
+        .call(d3.axisLeft(yScale));
+
+    // vertical grid lines
+    // chart.append('g')
+    //   .attr('class', 'grid')
+    //   .attr('transform', `translate(0, ${height})`)
+    //   .call(makeXLines()
+    //     .tickSize(-height, 0, 0)
+    //     .tickFormat('')
+    //   )
+
+    chart.append('g')
+        .attr('class', 'grid')
+        .call(makeYLines()
+            .tickSize(-width, 0, 0)
+            .tickFormat('')
+        )
+
+    const barGroups = chart.selectAll()
+        .data(product_data)
+        .enter()
+        .append('g')
+
+    barGroups
+        .append('rect')
+        .attr('class', 'bar')
+        .attr('x', (g) => xScale(g.review_date))
+        .attr('y', (g) => yScale(g.aggr_weighted_sentiment_score))
+        .attr('height', (g) => height - yScale(g.aggr_weighted_sentiment_score))
+        .attr('width', xScale.bandwidth())
+        .on('mouseenter', function (actual, i) {
+            d3.selectAll('.value')
+                .attr('opacity', 0)
+
+            d3.select(this)
+                .transition()
+                .duration(300)
+                .attr('opacity', 0.6)
+                .attr('x', (a) => xScale(a.review_date) - 5)
+                .attr('width', xScale.bandwidth() + 10)
+
+            const y = yScale(actual.aggr_weighted_sentiment_score)
+
+            line = chart.append('line')
+                .attr('id', 'limit')
+                .attr('x1', 0)
+                .attr('y1', y)
+                .attr('x2', width)
+                .attr('y2', y)
+
+            barGroups.append('text')
+                .attr('class', 'divergence')
+                .attr('x', (a) => xScale(a.review_date) + xScale.bandwidth() / 2)
+                .attr('y', (a) => yScale(a.aggr_weighted_sentiment_score) + 30)
+                .attr('fill', 'white')
+                .attr('text-anchor', 'middle')
+                .text((a, idx) => {
+                    const divergence = (a.aggr_weighted_sentiment_score - actual.aggr_weighted_sentiment_score).toFixed(2)
+
+                    let text = ''
+                    if (divergence > 0) text += '+'
+                    text += `${divergence}`
+
+                    return idx !== i ? text : '';
+                })
+
+        })
+        .on('mouseleave', function () {
+            d3.selectAll('.value')
+                .attr('opacity', 1)
+
+            d3.select(this)
+                .transition()
+                .duration(300)
+                .attr('opacity', 1)
+                .attr('x', (a) => xScale(a.review_date))
+                .attr('width', xScale.bandwidth())
+
+            chart.selectAll('#limit').remove()
+            chart.selectAll('.divergence').remove()
+        })
+
+    barGroups
+        .append('text')
+        .attr('class', 'value')
+        .attr('x', (a) => xScale(a.review_date) + xScale.bandwidth() / 2)
+        .attr('y', (a) => yScale(a.aggr_weighted_sentiment_score) + 30)
+        .attr('text-anchor', 'middle')
+        .text((a) => `${a.aggr_weighted_sentiment_score}`)
+
+    svg
+        .append('text')
+        .attr('class', 'label')
+        .attr('x', -(height / 2) - margin)
+        .attr('y', margin / 2.4)
+        .attr('transform', 'rotate(-90)')
+        .attr('text-anchor', 'middle')
+        .text('Weighted Sentiment Score')
+
+    svg.append('text')
+        .attr('class', 'label')
+        .attr('x', width / 2 + margin)
+        .attr('y', height + margin * 1.7 + 20)
+        .attr('text-anchor', 'middle')
+        .text('Review Date')
+
+    svg.append('text')
+        .attr('class', 'title')
+        .attr('x', width / 2 + margin)
+        .attr('y', 40)
+        .attr('text-anchor', 'middle')
+        .text('Sentiment Trends')
+
+    svg.append('text')
+        .attr('class', 'source')
+        .attr('x', width - margin / 2)
+        .attr('y', height + margin * 1.7)
+        .attr('text-anchor', 'start')
+        //.text('Source: Amazon Reviews')
+
+}
+
+function showProducts() {
+    
 }
